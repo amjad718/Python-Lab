bbd6c248aa41dd4badd20fdb5c8cf08903db7058
amjad <amjad.hussain718@gmail.com>
2023-01-10 23:36:12 +0530

    Updated Bank Question

--- C04/2) Bank Account.py
+++ C04/2) Bank Account.py
@@ -12,6 +12,9 @@ class Bank:
         print("The balance in your account is ", self.balance)
         print("Account Details \n 1)Account Number:", accnum, "\n 2)Bank Name",accname, "\n 3)Account Type", acctype, "\n 4)Old Balance", accbalance,"\n 5)New Balance",self.balance,"\n")
 
+    def get_balance(self):
+        return self.balance
+
     def withdraw(self):
         self.money = int(
             input("Enter the amount of money you want to deposit from the bank"))
@@ -24,23 +27,24 @@ class Bank:
             print("Account Details \n 1)Account Number:", accnum, "\n 2)Bank Name",accname, "\n 3)Account Type", acctype, "\n 4)Old Balance", accbalance,"\n 5)New Balance",self.balance,"\n")
 
 
-flag=1
-print("OPTIONS \n 1)Enter the account details \n 2) Deposit \n 3) Withdraw \n 4)View the current bank details\n 0)Exit")
-while(flag!=0):
-    flag = int(input("Enter the choice"))
-    match flag:
-        case 1:
-            accnum = int(input("Enter your account number")) 
-            accname = input("Enter the name of your bank")
-            acctype = input("Enter the type of your account (Current/Savings)")
-            accbalance = int(input("Enter the balance of your account"))
-            acc = Bank(accnum, accname, acctype, accbalance)
-        case 2:
-            acc.deposit()
-        case 3:
-            acc.withdraw()
-        case 4:
-            print("Account Details \n 1)Account Number:", accnum, "\n 2)Bank Name",accname, "\n 3)Account Type", acctype, "\n 4)Balance",accbalance,"\n")
-        case _:
-            print("Invalid Choice")
 
+accnum = int(input("Enter your account number")) 
+accname = input("Enter the name of your bank")
+acctype = input("Enter the type of your account (Current/Savings)")
+accbalance = int(input("Enter the balance of your account"))
+print("Acoount Created Successfully !!")
+acc = Bank(accnum, accname, acctype, accbalance)
+
+flag=True
+while(flag):
+    print("OPTIONS \n 1) Deposit \n 2) Withdraw \n 3)View the current bank details\n 0)Exit")
+    choice = int(input("Enter the choice"))
+    if(choice==1):
+        acc.deposit()
+    if(choice==2):
+        acc.withdraw()
+    if(choice==3):
+        print("Account Details \n 1)Account Number:", accnum, "\n 2)Bank Name",accname, "\n 3)Account Type", acctype, "\n 4)Balance",Bank.get_balance(acc),"\n")
+    if(choice==0):
+        flag=False
+        break;

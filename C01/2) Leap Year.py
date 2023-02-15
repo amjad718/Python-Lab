a17ad490a9d653869a7f6700c2f6168d8b15ea05
amjad718 <amjad.hussain718@gmail.com>
2023-02-16 05:07:25 +0530

    Improved code structure

--- C01/2) Leap Year.py
+++ C01/2) Leap Year.py
@@ -1,6 +1,6 @@
 start=int(input("Enter the starting year"))
 end=int(input("Enter the ending year"))
-while(start<end):
+while(start<=end):
     if(start%4==0 and start%100!=0):
         print(start)
     if start % 100 == 0 and start % 400 == 0:
